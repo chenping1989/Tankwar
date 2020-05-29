e2c1eec64bacf062b1708f46e4ccc61bd1df9b0d
duke chen <duke.chen@vimall.com.sg>
2020-05-29 23:31:26 +0800

    增加精灵类

---
+++ modules/sprites/scenes.py
@@ -0,0 +1,68 @@
+'''
+Function:
+    场景元素类
+'''
+import pygame
+
+'''砖墙'''
+
+
+class Brick(pygame.sprite.Sprite):
+    def __init__(self, position, imagepath, **kwargs):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(imagepath)
+        self.rect = pygame.image.get_rect()
+        self.rect.left, self.rect.top = position
+
+
+'''铁墙'''
+
+
+class Iron(pygame.sprite.Sprite):
+    def __init__(self, position, imagepath, **kwargs):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(imagepath)
+        self.rect = pygame.image.get_rect()
+        self.rect.left, self.rect.top = position
+
+
+'''冰'''
+
+
+class Ice(pygame.sprite.Sprite):
+    def __init__(self, position, imagepath, **kwargs):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((24, 24))
+        for i in range(2):
+            for j in range(2):
+                self.image.blit(pygame.image.load(imagepath), (12 * i, 12 * j))
+        self.rect = pygame.image.get_rect()
+        self.rect.left, self.rect.top = position
+
+
+'''河流'''
+
+
+class River(pygame.sprite.Sprite):
+    def __init__(self, position, imagepath, **kwargs):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((24, 24))
+        for i in range(2):
+            for j in range(2):
+                self.image.blit(pygame.image.load(imagepath), (12 * i, 12 * j))
+        self.rect = pygame.image.get_rect()
+        self.rect.left, self.rect.top = position
+
+
+'''树'''
+
+
+class Tree(pygame.sprite.Sprite):
+    def __init__(self, position, imagepath, **kwargs):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((24, 24))
+        for i in range(2):
+            for j in range(2):
+                self.image.blit(pygame.image.load(imagepath), (12 * i, 12 * j))
+        self.rect = pygame.image.get_rect()
+        self.rect.left, self.rect.top = position
