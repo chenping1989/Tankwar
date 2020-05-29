43e58a32c7c0785471df1918ccead399463f955f
duke chen <duke.chen@vimall.com.sg>
2020-05-29 23:13:04 +0800

    增加精灵类

---
+++ modules/sprites/home.py
@@ -0,0 +1,28 @@
+'''
+Function:
+    大本营类
+'''
+import pygame
+
+'''大本营类'''
+
+
+class Home(pygame.sprite.Sprite):
+    def __init__(self, position, imagepaths, **kwargs):
+        pygame.sprite.Sprite.__init__(self)
+        self.imagepaths = imagepaths
+        self.image = pygame.image.load(self.imagepaths[0])
+        self.rect = pygame.image.get_rect()
+        self.rect.left, self.rect.top = position
+        self.alive = True
+
+    '''被摧毁'''
+
+    def setDead(self):
+        self.image = pygame.image.load(self.imagepaths[1])
+        self.alive = False
+
+    '''画到屏幕'''
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
